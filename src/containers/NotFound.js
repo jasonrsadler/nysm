ebb4d5aa6ea7516d29bcb1e810190b40c11346a7
Jason Sadler <jsadler@brave.com>
2018-09-15 16:11:57 -0400

    Moved app folder to root

--- src/containers/NotFound.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import '../style.css';
-
-export default () =>
-    <div className="NotFound site-title">
-        <h3>The page you requested does not exist --404</h3>
-    </div>
